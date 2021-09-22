3c47a511099a037aa955c83719c6f2b6d0de5c49
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2021-09-22 19:46:04 -0400

    Use session

--- formsapp/urls.py
+++ formsapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ReviewView, ThankyouView, ReviewListView, SingleReviewView
+from .views import ReviewView, ThankyouView, ReviewListView, SingleReviewView, FavoriteReview
 
 urlpatterns = [
     path("", ReviewView.as_view()),
     path('thankyou', ThankyouView.as_view()),
     path('reviewlist', ReviewListView.as_view()),
-    path('single-review/<int:pk>', SingleReviewView.as_view()),
+    path('single-review/favorite', FavoriteReview.as_view()),
+    path('single-review/<int:pk>', SingleReviewView.as_view()),    
 ]
--- formsapp/views.py
+++ formsapp/views.py
@@ -1,9 +1,11 @@
+from django.http.response import HttpResponseRedirect
 from formsapp.forms import ReviewsForm
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
 from .models import Review
 from .forms import ReviewsForm
+from django.views import View
 
 # Create your views here.
 
@@ -24,4 +26,15 @@ class ReviewListView(ListView):
 class SingleReviewView(DetailView):
     template_name = 'formsapp/single_review.html'
     model = Review
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['favorite'] = self.request.session.get('fav_review_id') == str(self.object.id)
+        return context
+
+class FavoriteReview(View):
+    def post(self, req): 
+        id = req.POST['id']
+        req.session['fav_review_id'] = id
+        return HttpResponseRedirect("/single-review/" + id)
     
